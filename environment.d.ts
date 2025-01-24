b35ca14ecd2fdb6987dd691619b38ec6bbdbbfba
Harsh <harshchamola@gmail.com>
2025-01-24 14:13:38 +0400

    final

--- environment.d.ts
+++ environment.d.ts
@@ -7,10 +7,6 @@ declare global {
       // neon db uri
       DATABASE_URL: string;
 
-      // uploadthing api key and app id
-      UPLOADTHING_SECRET: string;
-      UPLOADTHING_APP_ID: string;
-
       // app base url
       NEXT_PUBLIC_BASE_URL: string;
 
--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -4,9 +4,4 @@ import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  // config: {
-  //   uploadthingId: process.env.UPLOADTHING_APP_ID!,
-  //   uploadthingSecret: process.env.UPLOADTHING_SECRET!,
-  //   callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/api/uploadthing`,
-  // },
 });
